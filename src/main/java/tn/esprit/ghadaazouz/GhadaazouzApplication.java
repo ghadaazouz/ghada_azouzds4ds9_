6b25debf4d8ec2c76f8408c8d2fe54a6c00d2604
ghada <ghada.azouz@esprit.tn>
2026-02-06 10:34:49 +0100

    first push

---
+++ src/main/java/tn/esprit/ghadaazouz/GhadaazouzApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.ghadaazouz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhadaazouzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhadaazouzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/ghadaazouz/GhadaazouzApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.ghadaazouz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhadaazouzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
